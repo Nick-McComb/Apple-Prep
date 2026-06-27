c51c9840ba2fd2199a4bb23230993327d6225d87
Nicholas L McComb <nick.mccomb062@gmail.com>
2026-06-26 21:04:46 -0400

    fixed conflict messages

--- Git/Rebase-Prac.cpp
+++ Git/Rebase-Prac.cpp
@@ -1,11 +1,7 @@
 using namespace std;
 #include <iostream>
 
-<<<<<<< HEAD
-int value = 150;
-=======
 int value = 200;
->>>>>>> 9bea42e (changed the value of value to 200, forcing a conflict with main)
 
 int main(){
     cout << value + value << endl;
