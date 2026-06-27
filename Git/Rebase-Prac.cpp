ecf3217c8d1abe91745dbc0979efb42c30c7a7a5
Nicholas L McComb <nick.mccomb062@gmail.com>
2026-06-26 20:43:31 -0400

    changed the value of value to 200, forcing a conflict with main

--- Git/Rebase-Prac.cpp
+++ Git/Rebase-Prac.cpp
@@ -1,7 +1,11 @@
 using namespace std;
 #include <iostream>
 
+<<<<<<< HEAD
 int value = 150;
+=======
+int value = 200;
+>>>>>>> 9bea42e (changed the value of value to 200, forcing a conflict with main)
 
 int main(){
     cout << value + value << endl;
