8cfdee04c94d2844ac94f2e4b8b51113fc6de5bf
Nicholas L McComb <nick.mccomb062@gmail.com>
2026-06-29 23:20:08 -0400

    first attempt coding objc, still learning classes

--- Objective-C/main.m
+++ Objective-C/main.m
@@ -1,6 +1,14 @@
 #import <stdio.h>
 
+@interface Dog
+@end
+
+@implementation Dog
+@end
+
+
 int main(int argc, char *argv[]) {
+    Dog* dog1 = [[Dog alloc] init];
     printf("Objective-C works!\n");
     return 0;
 }
